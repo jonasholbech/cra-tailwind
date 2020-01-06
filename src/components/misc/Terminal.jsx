b1de8ce9ed9056f18f19b83ae0aa24a42b90db94
Jonas Holbech <jofh@kea.dk>
2020-01-07 00:42:51 +0100

    playing with terminal, going to bed

--- src/components/misc/Terminal.jsx
+++ src/components/misc/Terminal.jsx
@@ -6,9 +6,11 @@ export default function Terminal({
   host = "ttys002",
   bashStart = "$",
   typing = false,
-  delay = 100
+  delay = 100,
+  OSstate = "default"
 }) {
   const [counter, setCounter] = useState(0);
+  const [currentOSstate, setOSstate] = useState(OSstate);
   useEffect(() => {
     if (typing && counter < children.length) {
       const interval = setInterval(() => {
@@ -18,8 +20,17 @@ export default function Terminal({
     }
   }, [typing, delay, counter, children.length]);
   const message = typing ? children.substring(0, counter) : children;
+  if (currentOSstate === "closed") {
+    return <></>;
+  }
+  if (currentOSstate === "minimized") {
+    return <></>;
+  }
+  if (currentOSstate === "maximized") {
+    return <></>;
+  }
   return (
-    <div className="w-full shadow-2xl subpixel-antialiased rounded h-64 bg-black border-black mx-auto">
+    <div className="w-full shadow-2xl subpixel-antialiased rounded bg-black h-64 border-black mx-auto">
       <div
         className="flex items-center h-6 rounded-t bg-gray-100 border-b border-gray-500 text-center text-black"
         id="headerTerminal"
@@ -27,19 +38,23 @@ export default function Terminal({
         <div
           className="flex ml-2 items-center text-center border-red-900 bg-red-500 shadow-inner rounded-full w-3 h-3"
           id="closebtn"
+          onClick={() => setOSstate("closed")}
         ></div>
         <div
           className="ml-2 border-yellow-900 bg-yellow-500 shadow-inner rounded-full w-3 h-3"
           id="minbtn"
+          onClick={() => setOSstate("minimized")}
         ></div>
         <div
           className="ml-2 border-green-900 bg-green-500 shadow-inner rounded-full w-3 h-3"
           id="maxbtn"
+          onClick={() => setOSstate("maximized")}
         ></div>
         <div className="mx-auto pr-16" id="terminaltitle">
           <p className="text-center text-sm">Terminal</p>
         </div>
       </div>
+
       <div
         className="pl-1 pt-1 h-auto  text-green-200 font-mono text-xs bg-black"
         id="console"
